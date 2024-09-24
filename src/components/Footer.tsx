6c8d909d862474bc2398bfeb76486aa6ad33661d
Felipe Barros <felipejonathan.dev@gmail.com>
2024-09-24 19:03:57 -0300

    feat: criação do componente Footer provisório
    TODO: Esse componente deverá ser alterado a medida que forem adicionados novas seções

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="h-8 flex justify-center items-center text-zinc-800 text-xs">
+            <span>© 2024, Felipe Jonathan</span>
+        </footer>
+    )
+}

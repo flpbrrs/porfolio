cc9f93e3118e7041ae96b8bd7bf742bac9060a8e
Felipe Barros <felipejonathan.dev@gmail.com>
2024-09-24 18:49:03 -0300

    feat: criação do componente header provisório
    TODO: Esse componente deve ser alterado quando a página crescer com a adição de novas seções

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import BrandLogo from "./BrandLogo";
+
+export default function Header() {
+    return (
+        <header className="h-16 flex justify-center px-4 items-center">
+            <BrandLogo/>
+        </header>
+    )
+}
